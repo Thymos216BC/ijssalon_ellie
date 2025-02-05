4fe11dc11dfaef4a7f4340f852c0b9fb8193735a
Kaiden <skittle503007@gmail.com>
2025-02-05 21:46:17 +0100

    huiswerk les 8

---
+++ algemene_functies.py
@@ -0,0 +1,9 @@
+def mijn_functie_1(x):
+    return x*x
+def mijn_functie_2(a,b):
+    uitvoer_list = []
+    uitvoer_list.append(a+b)
+    uitvoer_list.append(a-b)
+    uitvoer_list.append(a*b)
+    uitvoer_list.append(a/b)
+    return uitvoer_list
