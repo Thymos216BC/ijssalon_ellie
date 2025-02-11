c32d0938a2015a415ccecafb8ff4c72571bc7965
Kaiden <skittle503007@gmail.com>
2025-02-12 00:40:23 +0100

    hw les 10

--- presentatie.py
+++ presentatie.py
@@ -1,3 +1,8 @@
+mijn_dict = {'vis':10,
+             'vlees':25,
+             'overig':15}
+totaal = 50
+
 def presenteer(dictionary,totaal):
     uitvoer = []
     for key,value in dictionary.items():
